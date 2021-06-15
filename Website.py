28846c5868a3eea4b424170ec5be783099f2d60d
Parag <paragdurafe@outlook.com>
2021-06-16 01:50:55 +0530

    Files Updated

--- Website.py
+++ Website.py
@@ -1,11 +1,17 @@
 from flask import Flask,render_template
 app=Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+@app.route('/products/')
+def product():
+    return render_template('product.html')
+
+@app.route('/Contact-us/')
+def contact():
+    return render_template('contact.html')
+
 if __name__=='__main__':
     app.run(debug=True)
